20f8037f2457238848f4e5d3d3a68ef539938b0b
petitBiscuits <loic.frossard01@gmail.com>
2025-06-04 19:41:38 +0200

    fix ProgressBar.tsx

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -4,10 +4,19 @@ import {RootState} from "../../hooks/store.tsx";
 function ProgressBar() {
     const currentTaskNumber = useSelector((state:RootState) => state.pipeline.currentTaskNumber);
 
+    const widthMap: { [key: number]: string } = {
+        0: "w-0",
+        1: "w-1/5",
+        2: "w-2/5",
+        3: "w-3/5",
+        4: "w-4/5",
+        5: "w-full",
+    };
+
     return (
         <>
             <div className={"h-2 bg-gray-300 rounded-full mx-16"}>
-                <div className={`h-full bg-black rounded-full ${currentTaskNumber === 0 ? "w-0" : "w-" + currentTaskNumber + "/5"}`}>
+                <div className={`h-full bg-black rounded-full ${widthMap[currentTaskNumber]}`}>
                 </div>
             </div>
         </>
